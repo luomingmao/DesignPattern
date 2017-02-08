e22234ce66d1873e0710de1e83b106c9ddf2539a
luomingmao <luomingmao@gmail.com>
2017-02-08 14:57:14 +0800

    First Version

---
+++ 02_AbstractFactory/AbstractFactory.h
@@ -0,0 +1,15 @@
+//AbstractFactory.h
+
+#ifndef _ABSTRACTFACTORY_H_
+#define _ABSTRACTFACTORY_H_
+
+class AbstractProductA;
+class AbstractProductB;
+
+class AbstractFactory
+{
+public:
+    virtual ~AbstractFactory();
+};
+
+#endif //~_ABSTRACTFACTORY_H_
---
+++ 02_AbstractFactory/Product.cpp
@@ -0,0 +1,66 @@
+//Product.cpp
+
+#include "Product.h"
+#include <iostream>
+
+using namespace std;
+
+AbstractProductA::AbstractProductA()
+{
+
+}
+
+AbstractProductA::~AbstractProductA()
+{
+
+}
+
+AbstractProductB::AbstractProductB()
+{
+
+}
+
+AbstractProductB::~AbstractProductB()
+{
+
+}
+
+ProductA1::ProductA1()
+{
+    count << "Product A1..." << endl;
+}
+
+ProductA1::~ProductA1()
+{
+
+}
+
+ProductA2::ProductA2()
+{
+    count << "Product A2..." << endl;
+}
+
+ProductA2::~ProductA2()
+{
+
+}
+
+ProductB1::ProductB1()
+{
+    cout << "Product B1..." << endl;
+}
+
+ProductB1::~ProductB1()
+{
+
+}
+
+ProductB2::ProductB2()
+{
+    cout << "Product B2..." << endl;
+}
+
+ProductB2::~Product()
+{
+    
+}
